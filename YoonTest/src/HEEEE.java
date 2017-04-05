7c15f48de3f86977199215321f9cd998f72e2a4f
MK-Jang <boadline@gmail.com>
2017-04-05 15:49:47 +0900

    33
    33

--- YoonTest/src/HEEEE.java
+++ YoonTest/src/HEEEE.java
@@ -20,11 +20,11 @@ public class HEEEE {
 	}
 
 	/**
-	 * @param args
+	 * @param args 
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.prinln("1");
 	}
 
 }
