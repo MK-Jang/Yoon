31c7c15ef9e4c6ebad3875bebfcd4cbd91456357
MK-Jang <boadline@gmail.com>
2017-04-13 17:00:09 +0900

    ddd

--- YoonTest/src/HEEEE.java
+++ YoonTest/src/HEEEE.java
@@ -24,10 +24,7 @@ public class HEEEE {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("12222");
-		
-		System.out.println("dd");
-		
+		System.out.println("1");
 	}
 
 }
